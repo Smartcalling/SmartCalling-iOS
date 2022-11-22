b875b5f3fb74103075094021acca1cfb8016ea7c
Ian Kevan <49951236+IanKevan@users.noreply.github.com>
2022-11-22 11:01:34 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "SmartCalling",
-      url: "https://github.com/Smartcalling/SmartCalling-iOS/releases/download/1.6.4/SmartCalling.zip",
-      checksum: "03b39ab1ec192650944f37749583e8e7f5b2e0849186a083470121321ab39e83"
+      url: "https://github.com/Smartcalling/SmartCalling-iOS/releases/download/1.6.7/SmartCalling.zip",
+      checksum: "e24b02690afbb9ba822df2bc3b80b29afd124842eed5e4fab3e011c28c5aadc4"
     )
   ]
 )
